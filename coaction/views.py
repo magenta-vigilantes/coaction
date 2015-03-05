0655b347c052c1e30ecd3b34228f58d418f3de11
Dean Fitzgerald <classicmurph@gmail.com>
2015-03-05 16:04:37 -0500

    Added schema model with Marshmallow

--- coaction/views.py
+++ coaction/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, flash
-
+from .models import Task, TaskSchema
 
 coaction = Blueprint("coaction", __name__, static_folder="./static")
-
+task_schema = TaskSchema()
 
 @coaction.route("/")
 def index():
