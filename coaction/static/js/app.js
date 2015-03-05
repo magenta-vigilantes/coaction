716afa1dc4a14be9314a0cfbb5545b7d3398867b
Bobby Erickson <ericksonr07@gmail.com>
2015-03-05 16:47:31 -0500

    made task-service file

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -41,9 +41,45 @@ app.config(['$routeProvider', function($routeProvider) {
 
 app.factory('taskService', ['$http', '$log', function($http, $log){
 
-  
+  function get(url) {
+    return processAjaxPromise($http.get(url));
+  }
 
-}])
+  function post(url, task) {
+    return processAjaxPromise($http.post(url, task));
+  }
+
+  function remove(url) {
+    return processAjaxPromise($http.delete(url));
+  }
+
+  function processAjaxPromise(promise) {
+    return promise.then(function (result) {
+      return result.data;
+    })
+    .catch(function (error) {
+      $log.log(error);
+    });
+  }
+
+  return {
+    getTaskList: function() {
+      return get('api/tasks');
+    },
+
+    getTask: function(id) {
+      return get('api/tasks' + id);
+    },
+
+    addTask: function(task) {
+      return post('api/tasks', task);
+    },
+
+    deleteTask: function(id) {
+      return remove('/api/res/' + id);
+    }
+  };
+}]);
 
 app.controller('Error404Ctrl', ['$location', function ($location) {
   this.message = 'Could not find: ' + $location.url();
