689515c367cb76d7308e8d37f748538886f79f20
Dean Fitzgerald <classicmurph@gmail.com>
2015-03-06 12:45:59 -0500

    created seed function to generate fake tasks

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask.ext.migrate import MigrateCommand
 from flask.ext.script.commands import ShowUrls, Clean
 
 from coaction import create_app, db
-
+from coaction.models import Task
 
 app = create_app()
 manager = Manager(app)
@@ -30,6 +30,20 @@ def createdb():
     """Creates the database with all model tables. 
     Migrations are preferred."""
     db.create_all()
+
+
+@manager.command
+def seed_tasks():
+    tasks = [("Fake data", "new", "2015-03-10T17:54:48.972000+00:00"),
+             ("Make new friends" "new", "2015-03-10T17:54:48.972000+00:00"),
+             ("Continue to be really, really cool", "new", "2015-03-10T17:54:48.972000+00:00")]
+    for title, status, due_date in tasks:
+        task = Task(title=title,
+                    status=status,
+                    due_date=due_date)
+        db.session.add(task)
+    db.session.commit()
+    print("tasks seeded")
 
 
 if __name__ == '__main__':
