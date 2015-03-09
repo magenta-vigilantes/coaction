09f0fff6a84d58ff6a55fa9ad1128220e8993df0
Bobby Erickson <ericksonr07@gmail.com>
2015-03-08 22:20:49 -0400

    working on logout still

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -32,6 +32,27 @@ app.config(['$routeProvider', function($routeProvider){
   }
 }])
 
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/logout/', {
+    controller: 'LogoutCtrl',
+    controllerAs: 'vm',
+    templateUrl: 'api/logout/'
+  });
+}])
+.controller('LoginCtrl', ['$location', 'User', 'userService', function($location, User, userService){
+  var self = this;
+
+  self.user = User();
+
+  self.goToLogin = function() {
+    $location.path('/login');
+  };
+
+  self.logoutUser = function(){
+    userService.loginUser(self.user).then(self.goToLogin);
+  }
+}])
+
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/tasks/new', {
     controller: 'NewTaskCtrl',
